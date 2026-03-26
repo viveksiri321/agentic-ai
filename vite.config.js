04e2eb3d3c5983b9651f6b7a9c94140bf4487400
viveksiri321 <217744939+viveksiri321@users.noreply.github.com>
2026-03-27 01:16:35 +0530

    fix: allow all hosts for render

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: process.env.PORT || 5173,
-    allowedHosts: 'all' // Sabhi hosts ko allow karne ke liye
+    allowedHosts: 'all' 
   },
   preview: {
     host: '0.0.0.0',
-    port: process.env.PORT || 5173,
-    allowedHosts: 'all' // Deployment (Preview) mode ke liye bhi allow karein
+    allowedHosts: 'all'
   }
 })
